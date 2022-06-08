14e4a809963ea5a16274b6cc11c80a4e55bc55eb
EC2 Default User <ec2-user@ip-172-31-46-60.ap-northeast-1.compute.internal>
2022-06-08 12:47:21 +0000

    料理の新規登録（バリデーション表示させる（料理名、材料、作り方))

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,4 +1,9 @@
 class Cook < ApplicationRecord
   # 料理モデルと画像を紐付けた
   mount_uploader :image, ImageUploader
+
+  # バリデーション(料理名、材料、作り方　必須)
+  validates :name, presence: true
+  validates :ingredient, presence: true
+  validates :recipe, presence: true
 end
