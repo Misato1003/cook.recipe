5b1355aba36574214f654a31c9211f50f7c667ee
EC2 Default User <ec2-user@ip-172-31-46-60.ap-northeast-1.compute.internal>
2022-06-13 07:29:57 +0000

    modelのrspec(cook,post,user)

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "レビューを登録する際、バリデーションが効いているかの確認" do
+    before do
+      @user = FactoryBot.create(:user, email: 'sample@com.jp')
+      @cook = FactoryBot.create(:cook)
+    end
+
+    it "レビューを登録する際、user_id, cook_idがあれば、有効である" do
+      post = build(:post, user_id: @user.id, cook_id: @cook.id)
+      expect(post).to be_valid
+    end
+
+    it "user_idがnilの場合、無効である" do
+      post = build(:post, user_id: nil, cook_id: @cook.id)
+
+      expect(post.valid?).to eq(false)
+    end
+
+    it "cook_idがnilの場合、無効である" do
+      post = build(:post, user_id: @user.id, cook_id: nil)
+
+      expect(post.valid?).to eq(false)
+    end
+
+    it "cook_id, user_idがnilの場合、無効である" do
+      post = build(:post, user_id: nil, cook_id: nil)
+
+      expect(post).not_to be_valid
+    end
+
+    it "content,nicknameがnilの場合、有効である" do
+      post = build(:post, user_id: @user.id, cook_id: @cook.id, content: "nil", nickname: "nil")
+
+      expect(post).to be_valid
+    end
+  end
+end
