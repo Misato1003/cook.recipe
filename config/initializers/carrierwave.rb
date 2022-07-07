cba7888ee0ec18e0641875e15014ccbd5ba6ede3
EC2 Default User <ec2-user@ip-172-31-46-60.ap-northeast-1.compute.internal>
2022-07-07 07:18:37 +0000

    Excon::Error::Forbiddenのエラーのため、carrierwave.rbの記述を変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     }
     config.fog_directory = ENV['AWS_BUCKET']
-     config.fog_public = true
+    config.fog_public = false
   end
 end
